ed375c35305731abdfc3acb4a469cd330816c74f
Xavitheforce <javier.miguelez.yague@gmail.com>
2021-12-20 18:33:21 +0100

    Update Estrella.py

--- Estrella.py
+++ Estrella.py
@@ -2,3 +2,7 @@ import random
 import turtle
 
 puntas = int(input("Eliga el número de puntas de su estrella a dibujar: "))
+def turtle_star(puntas, tamaño = 100):
+    dim_angulo = 360 / puntas
+    turtle.fillcolor('black')
+    turtle.begin_fill()
