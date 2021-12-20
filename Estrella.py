61a090c1cddd201daacb1ac4a1ebcf3b149ae018
Xavitheforce <javier.miguelez.yague@gmail.com>
2021-12-20 18:37:11 +0100

    Update Estrella.py

--- Estrella.py
+++ Estrella.py
@@ -6,3 +6,17 @@ def turtle_star(puntas, tamaño = 100):
     dim_angulo = 360 / puntas
     turtle.fillcolor('black')
     turtle.begin_fill()
+    if puntas % 2 == 0:
+        pclave = []
+        for i in range(0, puntas):
+            turtle.penup()
+            pclave.append(turtle.pos())
+            turtle.circle(tamaño, dim_angulo)
+        for j in range(0, len(pclave)):
+            if j % 2 == 0:
+                turtle.pendown()
+                turtle.goto(pclave[j][0], pclave[j][1])
+            else:
+                continue
+        turtle.goto(pclave[0][0], pclave[0][1])
+        turtle.penup()
